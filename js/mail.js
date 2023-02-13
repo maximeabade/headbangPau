913921326478ef52b5bcae0d81eadc13937f13c0
maximeabade <maximeabade@gmail.com>
2023-02-13 15:58:20 +0100

    EDIT OF MAIL CONTENT

--- js/mail.js
+++ js/mail.js
@@ -8,26 +8,15 @@ function sendMail() {
     var firstNameContact = document.getElementById("firstNameContact").value;
     var emailContact = document.getElementById("emailContact").value;
     var genreContact = document.getElementById("genreContact").value;
-    if (genreContact == "Monsieur") {
-        var genreContact = "M.";
-    } else if (genreContact == "Madame") {
-        var genreContact = "Mme";
-    }
-    else{
-        var genreContact = "";
-    }
-
+    if(genreContact == "Monsieur") {genreContact = "M.";}else if(genreContact == "Madame"){genreContact = "Mme";} else{genreContact = "";}
     var birthDateContact = document.getElementById("birthDateContact").value;
     var roleBusiness = document.getElementById("roleBusiness").value;
     var subject = document.getElementById("subject").value;
-    var message = document.getElementById("message").value+ "<br><br><br><br><br><br><br><br>" + genreContact + " " + firstNameContact + " " + nameContact + " né(e) le " + birthDateContact + " est " + roleBusiness + " et souhaite être contacté(e) à l'adresse suivante : " + emailContact + " le " + dateContact + ".\n" + "Cette personne a pour sujet : " + subject + ".\n" ;
-    //controle de recup de data
-    //console.log("data -->>"+dateContact+" "+nameContact+" "+firstNameContact+" "+emailContact+" "+genreContact+" "+birthDateContact+" "+roleBusiness+" "+subject+" "+message);
-
+    var message = document.getElementById("message").value+ "<br><br><br><br><br><br><br><br>" +
+    genreContact + " " + firstNameContact + " " + nameContact + " né(e) le " + birthDateContact + " est " + roleBusiness + " et souhaite être contactée à l'adresse suivante : " + emailContact +
+    "et a envoyé un mail le " + dateContact + "." + "<br>" + "Cette personne a pour sujet : " + subject + "."
+    + "<br><br><br><br> Service de contact de HEADBANG-PAU";
     //now we send a mail with the data thanks to the smtp js inclusion
-
-//email pswd: 86ECE90F7D4710B773632A450F1B37C6BB19, encrypt is a13e5f50-83f4-4f32-9f64-e57c65ed8856 
-
     Email.send({
         SecureToken : "a13e5f50-83f4-4f32-9f64-e57c65ed8856",
         To : emailContact,
