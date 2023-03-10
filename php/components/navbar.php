30a765370bc4249d8b0e14ab1df41e9db9b19276
maximeabade <maximeabade@gmail.com>
2023-03-10 16:32:25 +0100

    adds of php compos

---
+++ php/components/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#myPage">
+        <p>HEADBANG PAU</p>
+      </a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#who">QUI?</a></li>
+        <li><a href="#whofor">POUR QUI?</a></li> 
+        <li><a href="#guitares">GUITARES</a></li>
+        <li><a href="#micros">MICROS</a></li>
+        <li><a href="#batteries">BATTERIES</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ php/index.php
@@ -0,0 +1,9 @@
+<php
+    require_once './components/navbar.php';
+    require_once './components/accueil.php';
+    require_once './components/guitares.php';
+    require_once './components/micros.php';
+    require_once './components/batteries.php';
+    require_once './components/contact.php';
+    require_once './components/footer.php';
+?>
